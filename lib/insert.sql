7971c76306fe576d24318ea907f71bf008b835e5
Codey Pinero <codey.pinero@gmail.com>
2019-04-11 20:11:50 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'male', 'black', 'cuddly', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'male', 'black and white', 'apphrehensive', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, 'female', 'brown', 'loving', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'male', 'green and brown', 'serious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 4, 'female', 'white', 'happy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'male', 'gray', 'grumpy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 3, 'female', 'red and white', 'emotional', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 17, 'male', 'purple and orange', 'unstable', 1);
